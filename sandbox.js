ff52e9ec53c3d96b4beef0621cd5f08068ec9df7
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-09-01 15:44:11 +1000

    storing chatgpt response for dynamically written 3x3 grid

--- sandbox.js
+++ sandbox.js
@@ -9,3 +9,27 @@ for (let i = 0; i < 3; i++) {
 }
 
 console.log(matrix);
+
+{
+  /* <React.Fragment>
+  <div className="status">{status}</div>
+  {Array(3)
+    .fill(null)
+    .map((_, rowIndex) => (
+      <div className="board-row" key={rowIndex}>
+        {Array(3)
+          .fill(null)
+          .map((_, colIndex) => {
+            const squareIndex = rowIndex * 3 + colIndex;
+            return (
+              <Square
+                key={squareIndex}
+                value={squares[squareIndex]}
+                onSquareClick={() => handleClick(squareIndex)}
+              />
+            );
+          })}
+      </div>
+    ))}
+</React.Fragment>; */
+}
